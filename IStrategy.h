767fed685e83fd0399404046fd95e668039d7715
SheiinX <82066007+SheiinX@users.noreply.github.com>
2022-10-19 11:44:48 -0500

    Try in virtual table

--- IStrategy.h
+++ IStrategy.h
@@ -1,10 +1,23 @@
 #ifndef _ISTRATEGY_H
 #define _ISTRATEGY_H
 
-typedef struct _istrategy
+struct stratVtbl;
+typedef struct
 {
-    char (*list)(void *data);
-} IStrategy;
+    struct stratVtbl const *vptr;
+    
+    /*char (*list)(void *data);*/
+} Strategy;
+
+struct stratVtbl{
+    void (*list)(Strategy const * const data);
+}
+
+void IStrategy_ctor(Strategy * const data);
+
+static inline void Strategy_list(Strategy const * const data){
+    (*me->vptr->list)(data);
+}
 
 void DoAlgorithm(void *data);
 
